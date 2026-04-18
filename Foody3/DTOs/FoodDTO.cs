e60bad37716ef1edf41cd7042fbdb88b0e101f96
HristoKr <hristokrazev@gmail.com>
2026-04-18 03:30:38 +0300

    Added Nunit project

---
+++ Foody3/DTOs/FoodDTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Foody3.Tests.DTOs
+
+{
+    internal class FoodDTO
+    {
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public string Url { get; set; }
+
+    }
+}
+
+
